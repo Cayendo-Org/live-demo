9c08069d8249d3db3050ab4dbbf532423f634991
Julian de Rushe <julian.derushe@mail.utoronto.ca>
2021-10-20 19:07:44 -0400

    Update deployment script

--- coordinator/webpack.config.js
+++ coordinator/webpack.config.js
@@ -2,11 +2,20 @@ const path = require("path");
 const { NODE_ENV = "production" } = process.env;
 const nodeExternals = require("webpack-node-externals");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: "./src/index.ts",
   mode: NODE_ENV,
   target: "node",
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "../app/build/", to: "./build/" },
+        { from: "./package.json", to: "./package.json" },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+  ],
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "index.js",
