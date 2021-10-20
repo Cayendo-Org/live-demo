0ecb391cb04a8605b447618aca4e68e30411cee9
Julian de Rushe <julian.derushe@mail.utoronto.ca>
2021-10-20 18:33:21 -0400

    Change deployment scripts

--- coordinator/webpack.config.js
+++ coordinator/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const { NODE_ENV = "production" } = process.env;
 const nodeExternals = require("webpack-node-externals");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   entry: "./src/index.ts",
   mode: NODE_ENV,
   target: "node",
+  plugins: [new CleanWebpackPlugin()],
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "index.js",
